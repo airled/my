274d349978321725c61cb75dfada2d5d7758e46e
airled <airled@mail.ru>
2015-03-22 20:29:10 +0300

    for work

--- models_test.rb
+++ models_test.rb
@@ -1,17 +1,17 @@
 require 'sequel'
 require 'mysql2'
-require 'pry'
+#require 'pry'
 require 'logger'
 
 database=Sequel.connect(:adapter=>'mysql2', :host=>'localhost', :user=>'root', :database=>'test')
 
-database.loggers << Logger.new($stdout)
+#database.loggers << Logger.new($stdout)
 
 database.run('drop table if exists artists')
 database.create_table(:artists) do
 	primary_key :id
 	String :name
-	String :option
+	String :country
 end
 
 database.run('drop table if exists albums')
@@ -19,7 +19,6 @@ database.create_table(:albums) do
   primary_key :id
   Integer :artist_id
   String :name
-  String :option
 end
 
 class Artist < Sequel::Model
@@ -32,26 +31,33 @@ class Album < Sequel::Model
   many_to_one :artist
 end
 
-Artist.create(:name=>'q',:option=>'a')
-Artist.create(:name=>'w',:option=>'s')
-Artist.create(:name=>'e',:option=>'d')
-Artist.create(:name=>'r',:option=>'f')
-Artist.create(:name=>'t',:option=>'g')
+Artist.create(:name=>'Covenant',:country=>'Sweden')
+Artist.create(:name=>'Bad Sector',:country=>'Italy')
+Artist.create(:name=>'Lustmord',:country=>'UK')
+Artist.create(:name=>'Carbon Based Lifeforms',:country=>'Sweden')
+Artist.create(:name=>'Aesthetische',:country=>'Brazil')
 
-Album.create(:artist_id=>1,:name=>'y',:option=>'h')
-Album.create(:artist_id=>2,:name=>'u',:option=>'j')
-Album.create(:artist_id=>3,:name=>'i',:option=>'k')
-Album.create(:artist_id=>1,:name=>'o',:option=>'l')
-Album.create(:artist_id=>3,:name=>'p',:option=>':3')
-Album.create(:artist_id=>4,:name=>'yq',:option=>'hz')
-Album.create(:artist_id=>2,:name=>'uw',:option=>'jx')
-Album.create(:artist_id=>3,:name=>'ie',:option=>'kc')
-Album.create(:artist_id=>2,:name=>'or',:option=>'lv')
-Album.create(:artist_id=>1,:name=>'pt',:option=>':33')
+Album.create(:artist_id=>1,:name=>'Skyshaper')
+Album.create(:artist_id=>1,:name=>'Sequencer')
+Album.create(:artist_id=>1,:name=>'Europa')
+Album.create(:artist_id=>2,:name=>'Plasma')
+Album.create(:artist_id=>2,:name=>'CMASA')
+Album.create(:artist_id=>2,:name=>'Kosmodrom')
+Album.create(:artist_id=>3,:name=>'Metavoid')
+Album.create(:artist_id=>3,:name=>'Beyond')
+Album.create(:artist_id=>4,:name=>'Twentythree')
+Album.create(:artist_id=>4,:name=>'Interloper')
+Album.create(:artist_id=>5,:name=>'Powerswitch')
+Album.create(:artist_id=>5,:name=>'Hybridcore')
 
-@artist=Artist[:name=>'w']
+@artist=Artist[:name=>'Aesthetische']
+p @artist.albums_dataset.select(:name)
+@album=Album[:name=>'CMASA']
+p @album.artist
+#@artist.remove_all_albums
+#artist.add_album(:name=>'Northern Light')
 
-binding.pry
+#binding.pry
 =begin
 a=gets.chomp
 b=gets.chomp

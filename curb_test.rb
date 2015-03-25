ab43de2cb8f4042db0c5e52a8a5def5f4cceb7e1
airled <airled@mail.ru>
2015-03-25 20:42:39 +0300

    with omg

--- curb_test.rb
+++ curb_test.rb
@@ -2,16 +2,30 @@ require 'curb'
 require 'nokogiri'
 require 'open-uri'
 
-a=File.open('/home/air/Desktop/1.txt','w')
-b=[]
+file=File.open('/home/air/Desktop/1.txt','w')
+arr=[]
+#i=1
 puts
 doc = Nokogiri::HTML(open('http://www.catalog.onliner.by'))
-doc.xpath("//table[@class='fphotblock add_line_main_menu']//li").each do |link|
-	a.puts link.content
-	b.push link.content
-	#puts link.content
+#doc.xpath("//table[@class='fphotblock add_line_main_menu']//ul[@class='b-catalogitems']/li/a[1]/@href").each do |link|
+#/tbody/tr/td/div/ul/li
+doc.xpath("//table[@class='fphotblock add_line_main_menu']//div/a[1]/@href").each do |link|
+#doc.xpath("//table[@class='fphotblock add_line_main_menu']//li").each do |link|
+	arr.push link.value
+	#arr.push i.to_s+' - '+link
+	#i+=1
 end
-puts b.size
+puts arr.size
+arr=arr.sort
+
+i=1
+arr.map do |val|
+	arr[i-1]=i.to_s+' - '+arr[i-1]
+	i+=1
+end
+
+file.puts arr
+file.close
 =begin
 puts
 http = Curl.get("http://www.nokogiri.org/tutorials/installing_nokogiri.html")
---
+++ omg.rb
@@ -0,0 +1,27 @@
+require 'sequel'
+require 'mysql2'
+require 'curb'
+require 'nokogiri'
+require 'open-uri'
+
+doc=doc = Nokogiri::HTML(open('http://www.catalog.onliner.by'))
+#array of URLS
+urls=[]
+doc.xpath("//table[@class='fphotblock add_line_main_menu']//div/a[1]/@href").each do |link|
+	urls.push link.value
+end
+urls=urls.sort
+
+DB=Sequel.connect(:adapter=>'mysql2', :user=>'root', :host=>'localhost', :database=>'test')
+
+DB.create_table :urls do
+	primary_key :id
+	String :url
+end
+
+class Url < Sequel::Model
+end
+
+urls.map do |url|
+	Url.create(:url=>url)
+end

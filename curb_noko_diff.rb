b6912230911aadcf81e2668659ad59831adbee7d
airled <airled@mail.ru>
2015-04-23 11:33:10 +0300

    with diff

---
+++ curb_noko_diff.rb
@@ -0,0 +1,13 @@
+require 'nokogiri'
+require 'open-uri'
+require 'curb'
+
+def infile(html,file)
+  File.open(file,'w') { |x| x.puts html }
+end
+  
+html_noko = Nokogiri::HTML(open('http://ya.ru'))
+html_curl = Curl::Easy.perform("http://ya.ru").body
+
+infile(html_noko,'noko.html')
+infile(html_curl,'curb.html')

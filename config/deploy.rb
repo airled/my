896acdfaa59781d3de2e5859cad20434fed95302
airled <airled@mail.ru>
2015-04-25 19:29:49 +0300

    123

--- config/deploy.rb
+++ config/deploy.rb
@@ -61,8 +61,8 @@ task :deploy => :environment do
     # Put things that will set up an empty directory into a fully set-up
     # instance of your project.
     invoke :'git:clone'
-#    invoke :'deploy:link_shared_paths'
-echo_cmd('bundle install')
+    invoke :'deploy:link_shared_paths'
+#echo_cmd('bundle install')
 #    invoke :'bundle:install'
 #    invoke :'rails:db_migrate'
 #    invoke :'rails:assets_precompile'
